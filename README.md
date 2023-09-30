0e6c687ac8d18feac0ba0497912f960ca3ff08d2
kernelmustard <kaleb.kreuer@proton.me>
2023-09-30 10:35:15 -0400

    syntax preferences

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # detect-debugger
+## Using
+### Linux
+#### Anti-Static
+The point of this isn't "perfect" anti-static-analysis, but the following should be configured at build to make it nontrivial:
+1. CMake build target should be set to MinSizeRel, where it excludes debug info and optimizes for size
+2. `add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD COMMAND strip -R .comment ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME})` in CMakeLists.txt to strip the .comment section after every build
+3. `add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD COMMAND echo 'Ag==' | base64 -d | dd conv=notrunc of=${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME} bs=1 seek=5)` in CMakeLists.txt 
 ## Building
 The easiest way to build is via VSCode CMake Extension on the target platform. Otherwise it may take some finnagling.
 ### Manually
@@ -27,10 +34,6 @@ cmake ..; msbuild build/somefile # need to build on windows to get the actual na
 - [CTF Wiki: Detecting Breakpoints Bypassing](https://ctf-wiki.mahaloz.re/reverse/linux/detect-bp/)
 - [CodeBreakers 2006 - AntiDebugging Techniques](https://repo.zenk-security.com/Reversing%20.%20cracking/CodeBreakers%202006%20-%20AntiDebugging%20techniques.pdf)
 - [Programming Linux Anti-Reversing Techniques.pdf](https://samples.vx-underground.org/root/Papers/Linux/Evasion/2016-12-20%20-%20Programming%20Linux%20Anti-Reversing%20Techniques.pdf)
-## Planned Features
-- Additional x86_64 Linux anti-debugging
-- Windows x86_64 anti-debugging
-- Ability to selectively run tests
 ## Will Not Do
 ### Anti-VM Detection
 - Check out [this](https://github.com/a0rtega/pafish) great project by a0rtega called pafish for all your Anti-VM testing needs
--- src/lin/lin.c
+++ src/lin/lin.c
@@ -12,7 +12,6 @@
 
 // Attempt to debug parent via child process, if it fails you are being debugged
 int trace_self() {
-    unsigned int rval = 0;
     prctl(PR_SET_PTRACER, (unsigned long)getpid(), 0, 0, 0); // allow attach parent <--> child (default is parent --> child )
     int pid = fork();
     int status;
@@ -23,73 +22,67 @@ int trace_self() {
             waitpid(ppid, NULL, 0);   // wait for the parent to stop/continue it
             ptrace(PTRACE_CONT, NULL, NULL);
             ptrace(PTRACE_DETACH, ppid, NULL, NULL); // detach
-            rval = 0;   // not being debugged
+            exit(0);   // not being debugged
         } else {
-            rval = 1;   // being debugged
+            exit(1);   // being debugged
         }
-        exit(rval);
     } else {    // parent
         waitpid(pid, &status, 0);
-        rval = WEXITSTATUS(status);
+        return WEXITSTATUS(status);
     }
-    return rval;
 }
 
 // same as above, but try to attach via ptrace twice and modify a variable to detect if one has been patched
 int trace_self_withstate() {
-    unsigned int rval;
     int canary = 2; // this internal state mechanism is incredibly weak
 
-    if ((rval = trace_self()) == 0) {
+    if (trace_self() == 0) {
         canary = canary * 4;
     } else {    
-        rval = 1;
-        goto ret;
+        return 1;
     }
 
-    if ((rval = trace_self()) == 0) {
+    if (trace_self() == 0) {
         canary = canary * 2;
     } else {
-        rval = 1;
-        goto ret;
+        return 1;
     }
     
     if (canary == 2 * 4 * 2) {
-        rval = 0;   // not being debugged
+        return 0;   // not being debugged
     } else {
-        rval = 1;   // a ptrace was patched
+        return 1;   // a ptrace was patched
     }
-ret:
-    return rval;
 }
 
 // open /proc/self/status and find Tracer PID; if you are being traced then that PID is the tracing
 // process. Otherwise, it will be 0.
 int get_tracer_pid() {
-    unsigned int rval = 1;
     FILE *fp  = fopen("/proc/self/status", "r");
     char line[1024];
 
     if (fp != NULL) {
         while (fgets(line, sizeof(line), fp)) {
             if (strstr(line, "TracerPid:\t0") != NULL) {
-                rval = 0;
+                fclose(fp);
+                return 0;
             }
         }
         fclose(fp);
-    } // should add else statement with error message
-
-    return rval;
+        return 1;
+    } else {
+        perror("\x1b[31mERROR\x1b[0m");
+        return 1;
+    }
 }
 
 // read ppid in /proc/self/status, resolve /proc/(ppid)/exe (symlink), compare to known debugger binaries
 int match_parent_to_dbg() {
-    unsigned int rval = 0;  // honestly this technique sucks just change the name of your debugger lol
-    char *known_dbg[] = {   // better malware would obfuscate/encrypt these strings
+    char *known_dbg[] = {
         "gdb",
         "strace",
         "lldb-server"
-    };  // add more later if requested
+    };
     int listlen = sizeof(known_dbg) / sizeof(known_dbg[0]);
 
     // convert (int)ppid to (char *)ppid_s
@@ -118,22 +111,20 @@ int match_parent_to_dbg() {
     // compare name to list of known debuggers
     for (int i = 0; i < listlen; i++) {
         if (strncmp(name, known_dbg[i], (size_t)listlen) == 0) {
-            rval = 1;
+            return 1;
             break;
         }
     }
 
     free(ppid_s); 
     free(symlink);
-    return rval;
+    return 0;
 }
-
-void breakme() {  } // sample function to set breakpoints
 
 // measure difference in time at the beginning and end
 // if difference exceeds limit then program has been delayed artificially
+void breakme() {}   // sample function to set breakpoints
 int detect_execution_delay() {
-    unsigned int rval;
     struct timeval tp;
 
     gettimeofday(&tp, 0);
@@ -144,43 +135,34 @@ int detect_execution_delay() {
     gettimeofday(&tp, 0);
     long int end_ms = tp.tv_sec * 1000 + tp.tv_usec / 1000;
 
-    // for your viewing pleasure
-    //printf("%ld\n%ld\n", start_ms, end_ms);
-
     if ((end_ms - start_ms) > 0) {  // this value will need to be adjusted
-        rval = 1;                   // really meant to catch breaks
+        return 1;                   // really meant to catch breaks
     } else {
-        rval = 0;
+        return 0;
     }
-
-    return rval;
 }
 
 int bypass_breakpoints() {  // currently doesn't work, instruction is f3 regardless of breakpoint
-    unsigned int rval;
-    
+
     // check for int 3 at breakme() function entry
     if ((*(volatile unsigned int *)((uintptr_t)breakme) & 0xff) == 0xcc) {
         printf("breakpoint!\n");
-        rval = 1;
+        return 1;
     } else {
         //printf("addr is %x\n", *(volatile unsigned int *)((uintptr_t)breakme) & 0xff);
-        rval = 0;
+        return 0;
     }
     breakme();
-
-    return rval;
 }
 
 sig_atomic_t g_rval = 1; // detected by default
 void bp_handler() { g_rval = 0; }   // set if no debugger that handles SIGTRAP
 int fake_breakpoint() {
-    unsigned int rval;
-    unsigned int handler;
 
     signal(SIGTRAP, bp_handler);    // register custy SIGTRAP handler
     asm ("int3");                   // send SIGTRAP
 
-    rval = g_rval;  // there are a couple instructions that can raise a EXCEPTION_BREAKPOINT
-    return rval;    // int 3 is an example
+    // there are a couple instructions that can raise a EXCEPTION_BREAKPOINT
+    // int 3 is an example
+    return g_rval;    
 }
